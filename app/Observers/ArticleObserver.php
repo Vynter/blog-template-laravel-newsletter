788bac2745b209fda2de1b5dc92596e9f4c3e7a9
Vynter <amine-cheraitia@hotmail.com>
2021-06-03 01:40:20 +0200

    100%

---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Observers;
+
+use App\Article;
+use Cache;
+
+class ArticleObserver
+{
+    /**
+     * saved utilisé aprés update ou aprés création
+     *
+     * @param  mixed $article
+     * @return void
+     */
+    public function saved(Article $article)
+    {
+        Cache::flush();
+    }
+    /**
+     * Handle the article "created" event.
+     *
+     * @param  \App\Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the article "updated" event.
+     *
+     * @param  \App\Article  $article
+     * @return void
+     */
+    public function updated(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the article "deleted" event.
+     *
+     * @param  \App\Article  $article
+     * @return void
+     */
+    public function deleted(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the article "restored" event.
+     *
+     * @param  \App\Article  $article
+     * @return void
+     */
+    public function restored(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the article "force deleted" event.
+     *
+     * @param  \App\Article  $article
+     * @return void
+     */
+    public function forceDeleted(Article $article)
+    {
+        //
+    }
+}
